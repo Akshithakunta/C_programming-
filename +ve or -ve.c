cab005392bda152d33b0bfcd22aaf2e188a062bc
Akshithakunta <akshithapatel758@gmail.com>
2026-01-10 18:41:18 +0530

    Add files via upload

---
+++ +ve or -ve.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("enter the number \n");
+scanf("%d",&a);
+if(a>0)
+{
+printf("%d is +ve number\n",a);
+}
+else
+{
+printf("%d is -ve number\n",a);
+}
+}
